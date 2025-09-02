f00ab66442a95a8544169cd706d739ef0fc50103
Benno Brunner <b.brunner@reply.de>
2025-09-02 10:38:45 +0200

    Add service for http calls

---
+++ frontend/src/app/services/landmarks/landmarks.service.ts
@@ -0,0 +1,25 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HandLandmarkerResult} from '@mediapipe/tasks-vision';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LandmarksService {
+  private http = inject(HttpClient)
+
+
+  getCharacterPrediction(handLandmarkerResult: HandLandmarkerResult) {
+    const request = handLandmarkerResult.landmarks.map(landmarks => landmarks.map(landmark => ([landmark.x, landmark.y, landmark.z]))).flat()
+
+    return this.http.post<FingerAlphabetResponse>("/api/fingers", {
+      landmarks: request,
+      handedness: handLandmarkerResult.handedness[0][0].categoryName
+    })
+  }
+}
+
+interface FingerAlphabetResponse {
+  character: string;
+}
+
